9ba621a4bd9cb57ae0a27a7062d875ec8433fe10
Jean-Baptiste Camps <jbcamps@hotmail.com>
2019-01-07 13:17:08 +0100

    cleaning

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import re
 for i in glob.glob("*/*/*.gt.txt"):
      with open(i, 'r') as f:
          for line in f:
-             x = re.findall(r'[\u000A\u001A]', line)
+             x = re.findall(r'[\u000A\u001A\u0308]', line)
              #x = re.findall(r'[a]', line)
              if not x == []:
                   print(i+":")
